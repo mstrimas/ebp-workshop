7532ff4d1c6daf6637fe2b8185d9acd9bc98e4bb
Matt Strimas-Mackey <mstrimas@gmail.com>
2019-11-05 14:28:43 -0500

    part 2 intro

--- raw-data/workshop-data.R
+++ raw-data/workshop-data.R
@@ -2,8 +2,8 @@ library(auk)
 library(here)
 library(tidyverse)
 
-ebd_dir <- here("raw-data", "ebp-workshop-data", "ebd")
-data_dir <- here("raw-data", "ebp-workshop-data", "data")
+ebd_dir <- here("raw-data", "ebd")
+data_dir <- here("raw-data", "data")
 dir.create(ebd_dir, recursive = TRUE, showWarnings = FALSE)
 dir.create(data_dir, recursive = TRUE, showWarnings = FALSE)
 
@@ -22,13 +22,13 @@ f_ebd_workshop <- here("raw-data", "ebd_2014-2015_yucatan.txt")
 f_sed_workshop <- here("raw-data", "ebd_sampling_2014-2015_yucatan.txt")
 auk_ebd(f_ebd, f_sed) %>% 
   auk_state(c("MX-CAM", "MX-CHP", "MX-TAB", "MX-ROO", "MX-YUC")) %>% 
-  auk_filter(f_ebd_workshop, f_sed_workshop)
+  auk_filter(f_ebd_workshop, f_sed_workshop, overwrite = TRUE)
 # other countries
 f_ebd_nomx <- here("raw-data", "ebd_2014-2015_nomx.txt")
 f_sed_nomx <- here("raw-data", "ebd_sampling_2014-2015_nomx.txt")
 auk_ebd(f_ebd, f_sed) %>% 
   auk_country(c("Guatemala", "Belize")) %>% 
-  auk_filter(f_ebd_nomx, f_sed_nomx)
+  auk_filter(f_ebd_nomx, f_sed_nomx, overwrite = TRUE)
 
 # trim headers and combine
 str_glue("sed '1d' {f_ebd_nomx} >> {f_ebd_workshop}") %>% system()
@@ -42,9 +42,7 @@ f_ebd_workshop %>% file.copy(., file.path(ebd_dir, basename(.)))
 f_sed_workshop %>% file.copy(., file.path(ebd_dir, basename(.)))
 
 # part II files
-book_files <- c("ebd_woothr_june_bcr27.txt", 
-                "ebd_checklists_june_bcr27.txt",
-                "ebd_woothr_june_bcr27_zf.csv",
+book_files <- c("ebd_woothr_june_bcr27_zf.csv",
                 "gis-data.gpkg",
                 "mcd12q1_classes.csv",
                 "pland-elev_location-year.csv",
@@ -54,8 +52,11 @@ file.copy(file.path("~/projects/ebird-best-practices/data/", book_files),
           file.path(data_dir, book_files))
 
 # zip
-f_zip <- here("raw-data", "ebp-workshop-data.zip")
+f_zip <- "ebp-workshop-data.zip"
 unlink(f_zip)
-str_glue("zip -r {f_zip} {here('raw-data', 'ebp-workshop-data')}") %>% 
+str_glue("cd {here('raw-data')}; zip -r {f_zip} data/ ebd/") %>% 
   system()
-unlink(here("raw-data", "ebp-workshop-data"), recursive = TRUE)
+unlink(here("raw-data", "data"), recursive = TRUE)
+unlink(here("raw-data", "ebd"), recursive = TRUE)
+unlink(f_ebd_workshop)
+unlink(f_sed_workshop)
